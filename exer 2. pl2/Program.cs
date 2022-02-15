a7e738051afd9e34dd4899d587fad3d15eaf154a
senjuricardo <ricardo.sousa13@hotmail.com>
2022-02-15 21:45:33 +0000

    git vhnages

---
+++ exer 2. pl2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exer_2._pl2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
---
+++ exer1-pl2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace exer1_pl2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+           StreamReader rd = new StreamReader("D:\\VENCIMENTOS.txt");
+            
+            List<string> list = File.ReadAllLines("D:\\VENCIMENTOS.txt").ToList();
+
+            List<string> maiorMil = greaterThan(list, 1000);
+
+            if(maiorMil != null)
+            {
+               StreamWriter wr = new StreamWriter("D:\\SUPMIL.txt");
+                foreach(string linha in maiorMil)
+                {
+                    wr.WriteLine(linha);
+                    Console.WriteLine(linha);
+                }
+                wr.Close();
+            }
+
+        }
+        public static List<string> greaterThan(List<string> lines, int value)
+        {
+            List<string> result = new List<string>();
+            foreach (string line in lines)
+            {
+                string[] items = line.Split(' ');
+                if (int.Parse(items[2]) > value)
+                    result.Add(line);
+            }
+            return result;
+        }
+    }
+}
