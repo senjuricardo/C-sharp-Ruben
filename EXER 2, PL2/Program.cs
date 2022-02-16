211baf0a8c88e71bb84164c0c18d67194d678803
senjuricardo <ricardo.sousa13@hotmail.com>
2022-02-16 22:28:28 +0000

    COMMIT 16-2

---
+++ EXER 2, PL2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+namespace EXER_2__PL2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader rd = new StreamReader("D:\\NOTAS.txt");
+            StreamWriter wd = new StreamWriter("D:\\APROVADOS.txt");
+            StreamWriter wd40 = new StreamWriter("D:\\REPROVADOS.txt");
+
+            while(!rd.EndOfStream){
+                string linha = rd.ReadLine();
+                string[] palavras = linha.Split(' ');
+
+                    if (double.Parse(palavras[2]) >= 9.5)
+                    {
+                        wd.WriteLine(linha);
+                    }
+                    else
+                    {
+                        wd40.WriteLine(linha);
+                    }                     
+            }
+            rd.Close();
+            wd.Close();
+            wd40.Close();
+
+        }
+
+      
+    }
+    
+}
--- exer 2. pl2/Program.cs
+++ exer 2. pl2/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 
+using System.IO;
+
 namespace exer_2._pl2
 {
     class Program
     {
         static void Main(string[] args)
         {
-           
+            StreamReader rd = new StreamReader("D:\\VENCIMENTOS.txt");
+            StreamWriter wd = new StreamWriter("D:\\SUPMIL.txt");
+
+            while (!rd.EndOfStream)
+            {
+                string linha = rd.ReadLine();
+                string[] words = linha.Split(' ');
+
+               /* foreach (var word in words)
+                {
+                    System.Console.WriteLine($"<{word}>");
+                
+                }*/
+               if(int.Parse(words[2]) > 1000)
+                {
+                    wd.WriteLine(linha);
+                }
+            
+            }
+            rd.Close();
+            wd.Close();
+
+
         }
     }
 }
